621ae237f0f4ca07a2de807745175c7966835e2c
Morsheda <40772371+Morsheda@users.noreply.github.com>
2020-06-18 01:26:03 +0600

    commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,9 +13,28 @@ class _HomePageState extends State<HomePage> {
   final picker = ImagePicker();
 
   getImageFile(ImageSource source) async {
+    //Clicking or picking image
     var image = await picker.getImage(source: source);
+
+    File croppedFile = await ImageCropper.cropImage(
+      sourcePath: image.path,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9
+      ],
+      androidUiSettings: AndroidUiSettings(
+          toolbarTitle: 'Cropper',
+          toolbarColor: Colors.deepOrange,
+          toolbarWidgetColor: Colors.white,
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: false),
+    );
+
     setState(() {
-      _image = File(image.path);
+      _image = File(croppedFile.path);
     });
   }
 
